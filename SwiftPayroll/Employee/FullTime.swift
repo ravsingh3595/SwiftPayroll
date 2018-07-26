2f7b82e9c0442eee77373b69299dfaa258368b6f
Sonia Rani <>
2018-07-25 23:48:28 -0400

    Committing classes

---
+++ SwiftPayroll/Employee/FullTime.swift
@@ -0,0 +1,9 @@
+//
+//  FullTime.swift
+//  SwiftPayroll
+//
+//  Created by Sonia Rani on 2018-07-25.
+//  Copyright © 2018 RavSingh. All rights reserved.
+//
+
+import Foundation
---
+++ SwiftPayroll/Vehicle/Motorcycle.swift
@@ -0,0 +1,9 @@
+//
+//  Motorcycle.swift
+//  SwiftPayroll
+//
+//  Created by Sonia Rani on 2018-07-25.
+//  Copyright © 2018 RavSingh. All rights reserved.
+//
+
+import Foundation
