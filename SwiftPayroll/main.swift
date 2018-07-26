864dda9d854227012cae33c90656b68a34bc8ea8
user <ravneets47@gmail.com>
2018-07-25 20:53:01 -0400

    Initial Commit

---
+++ SwiftPayroll/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftPayroll
+//
+//  Created by user on 2018-07-25.
+//  Copyright © 2018 RavSingh. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
