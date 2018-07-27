ebb14cb38190ee8939f8d37df1ab0309ae850e71
Sonia Rani <>
2018-07-26 21:17:17 -0400

    committing csv file creation and json read

---
+++ SwiftPayroll/EmployeeJson.swift
@@ -0,0 +1,43 @@
+//
+//  EmployeeJson.swift
+//  SwiftPayroll
+//
+//  Created by Sonia Rani on 2018-07-26.
+//  Copyright © 2018 RavSingh. All rights reserved.
+//
+
+import Foundation
+
+struct EmployeeJson: Codable {
+    let id: Int
+    let name: String
+    let age: Int
+    let vehicle: Vehicle?
+    let type: String
+    
+    // intern
+    let schoolName: String?
+    
+    // part time
+    let rate: Float?
+    let hoursWorked: Int?
+    
+    // PartTime/Fixed
+    let fixedAmount: Int?
+    
+    // fulltime
+    let salary: Float?
+    let bonus: Float?
+    
+    // parttime/commissioned
+    let commissionPercent: Int?
+    
+    struct Vehicle: Codable{
+        let id: Int
+        let make: String
+        let plate: String
+        let type: String
+        let isLongDistance: Bool?
+    }
+}
+
--- SwiftPayroll/main.swift
+++ SwiftPayroll/main.swift
@@ -10,3 +10,89 @@ import Foundation
 
 print("Hello, World! Test")
 
+var employee: [Employee] = [Employee]()
+
+//To get current Directory
+let cwd = FileManager.default.currentDirectoryPath
+print("script run from:\n" + cwd)
+
+
+
+if let filePath = Bundle.main.url(forResource: "Employee", withExtension: "json"){
+//    let url = URL(string: filePath)!
+    
+    let jsonData = try! Data(contentsOf: filePath)
+    
+    let jsonDecoder = JSONDecoder()
+    let employees = try? jsonDecoder.decode(Array<EmployeeJson>.self,
+                                            from: jsonData)
+    
+    if let employeeArray = employees{
+        for employee in employeeArray{
+            print(employee.id)
+            if let make = employee.vehicle?.make{
+                print(make)
+            }
+        }
+        
+        do{
+            let csvObj = CSVFile()
+            csvObj.createCSV(from: employeeArray)
+        }
+    }
+    
+    /// ---- get dictionary ---- ///
+    do {
+        let data = try Data(contentsOf: filePath)
+        let jsonDict: [Dictionary<String, Any>] = try JSONSerialization.jsonObject(with: data as Data, options: JSONSerialization.ReadingOptions.allowFragments) as! [Dictionary<String, Any>]
+        print(jsonDict)
+        for employee in jsonDict{
+            if let id = employee["id"]{
+                print("id: \(id)")
+            }
+        }
+        let csvObj = CSVFile()
+//        csvObj.createCSV(from: jsonDict)
+    }
+    catch {
+        //Handle error
+        print("ERROR")
+    }
+}else{
+    print("FAIL-2")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+///---- conversion using coder ---------///
+/*
+ let jsonData = try! Data(contentsOf: filePath)
+ 
+ let jsonDecoder = JSONDecoder()
+ let employees = try? jsonDecoder.decode(Array<EmployeeJson>.self,
+ from: jsonData)
+ 
+ if let employeeArray = employees{
+ for employee in employeeArray{
+ print(employee.id)
+ if let make = employee.vehicle?.make{
+ print(make)
+ }
+ }
+ 
+ do{
+ let csvObj = CSVFile()
+ csvObj.createCSV(from: employeeArray)
+ }
+ }*/
+
